e3abb712a721795817b99667dff724454eb87ada
Josh Tan <jtan189@gmail.com>
2013-10-26 00:42:10 -0500

    Code fully commented. Bytes per line determined to be exactly 4; code updated to reflect this.

--- adder.c
+++ adder.c
@@ -4,11 +4,7 @@
   Project 1: Fork and Pipe
   10/25/13
 
-  Sources:
-  http://stackoverflow.com/questions/5248915/execution-time-of-c-program
-  http://www.tutorialspoint.com/c_standard_library/c_function_rewind.htm
-  http://www-ee.eng.hawaii.edu/~tep/EE160/Book/chap7/section2.1.2.html
-  http://stackoverflow.com/questions/5139213/count-number-of-line-using-c
+  Resources:
   http://www.cs.usfca.edu/~wolber/SoftwareDev/C/CStructs.htm
   http://rabbit.eng.miami.edu/info/functions/time.html
 */
@@ -39,8 +35,7 @@ double time_summation(char *filepath, int num_proc, int should_print);
 int is_num(char *str);
 
 // constant variables
-const int BUFFER_SIZE = 100;
-const int BYTES_PER_LINE = 6; // might be 4 <###\r\n> + 1 (since will append \0)
+const int BYTES_PER_LINE = 4; // 3 digits + '\n' char
 const char *FILE1 = "file1.dat";
 const char *FILE2 = "file2.dat";
 const char *FILE3 = "file3.dat";
@@ -48,6 +43,13 @@ const char *FILE4 = "file4.dat";
 
 int main(int argc, char *argv[])
 {
+    // exit if not provided with valid command-line parameters
+    if (argc > 3 || argc == 1) 
+    {
+        printf("Usage: %s <filepath> [<num executions>]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     // get name of input file
     char *filepath = argv[1];
 
@@ -56,18 +58,13 @@ int main(int argc, char *argv[])
     printf("Enter the number of parallel processes to utilize (1, 2, or 4):\n");
     scanf("%d", &num_proc);
 
-    // exit if not provided with valid command-line parameter(s)
-    if (argc > 3) 
-    {
-        printf("Usage: %s <filepath>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
+    // get number of times to execute summation logic
     int num_tests;
     if (argc == 3)
     {
 	if (is_num(argv[2]) == 0)
 	{
+	    // execution times parameter is non-numeric
 	    printf("Invalid number of executions specified.\n");
 	    exit(EXIT_FAILURE);
 	}
@@ -78,7 +75,7 @@ int main(int argc, char *argv[])
     }
     else
     {
-	num_tests = 1;
+	num_tests = 1; // default to single execution
     }
 
     printf("---------------------------------------------------------\n");
@@ -86,16 +83,19 @@ int main(int argc, char *argv[])
 
     int i;
     double total_time = 0;
-    int should_print = 0;
+    int should_print = 0; // flag used to print summation info only once per program invocation
     for (i = 0; i < num_tests; i++)
     {
         if (i == (num_tests - 1))
         {
+	    // only print summation info on last execution
             should_print = 1;
         }
+	// add the times for each execution
         total_time += time_summation(filepath, num_proc, should_print);
     }
 
+    // logic to adjust output based on number of executions
     if (num_tests == 1)
     {
         printf("Execution time: %f seconds\n", total_time);
@@ -105,16 +105,18 @@ int main(int argc, char *argv[])
         total_time = total_time / (double) num_tests;
         printf("Execution time: %f seconds (averaged over %d tests)\n", total_time, num_tests);
     }
+
     printf("---------------------------------------------------------\n");
     exit(EXIT_SUCCESS);
 }
 
+/*
+  Sum the numbers contained in the given file, using the specified number
+  of processes. If should_print is 1, then print the summation info
+  to standard output. Return the time taken to perform the summation.
+ */
 double time_summation(char *filepath, int num_proc, int should_print)
 {
-
-    int i;
-    int total_nums;
-
     // start timer
     struct timeval t1;
     struct timeval t2;
@@ -122,7 +124,8 @@ double time_summation(char *filepath, int num_proc, int should_print)
 
     char *filename = basename(filepath);
 
-    // initialize number of lines in file
+    // determine number of lines in file (hardcoded)
+    int total_nums;
     if (strcmp(filename, FILE1) == 0)
     {
         total_nums = 1000;
@@ -141,32 +144,40 @@ double time_summation(char *filepath, int num_proc, int should_print)
     }
     else
     {
-        printf("Error: unknown filename.\nValid filenames are 'file{1,2,3,4}.dat'.");
+        printf("Error: unknown filename.\nValid filenames are 'file{1,2,3,4}.dat'.\n");
         exit(EXIT_FAILURE);
     }
 
     // initialize child pipes
     int pipes_to_child[num_proc][2];
     int pipe_to_parent[num_proc][2];
+
+    int i;
     for (i = 0; i < num_proc; i++)
     {
         pipe(pipes_to_child[i]);
         pipe(pipe_to_parent[i]);
     }
 
+    /*
+      Fork the specified number of processes to calculate partial sums for the
+      file. The i'th process calculates the sum for the i'th section of the file,
+      where file sections are divided equally. Each processes send the partial
+      sum back to the parent.
+     */
     int proc_id;
     for (proc_id = 0; proc_id < num_proc; proc_id++)
     {
-        // can fork process to work on these
+        // create a child process to compute the partial sum
         pid_t fork_pid;
         fork_pid = fork();
-        if (fork_pid == 0)
+        if (fork_pid == 0) // is child process
         {
-            // get part of file to work on, from parent
+            // retrieve part of file to work on from parent
             file_portion child_portion;
             read(pipes_to_child[proc_id][0], &child_portion, sizeof(file_portion));
 
-            // calculate sum of portion
+            // calculate partial sum
             int child_sum = add_nums(child_portion);
 
             // send partial sum to parent
@@ -174,18 +185,21 @@ double time_summation(char *filepath, int num_proc, int should_print)
 
             exit(EXIT_SUCCESS);
         }
-        else
+        else // is parent process
         {
+	    // encapsulate file portion info in a struct
             file_portion portion;
             portion.filepath = filepath;
             portion.num_lines = total_nums  / num_proc;
-            portion.offset = proc_id * portion.num_lines * 4; // STOP HARD CODING FKDLSJFIO#WJRIPO
+            portion.offset = proc_id * portion.num_lines * BYTES_PER_LINE;
+
+	    // send info to appropriate child
             write(pipes_to_child[proc_id][1], &portion, sizeof(file_portion));
         }
     }
     
-    int total_sum;
-    total_sum = 0;
+    // add up all partial sums
+    int total_sum = 0;
     for (i = 0; i < num_proc; i++)
     {
         // read partial sum from child
@@ -194,14 +208,14 @@ double time_summation(char *filepath, int num_proc, int should_print)
         total_sum += child_sum;
     }
 
-    // if flag set, print total
+    // if flag set, print num lines processed and total sum
     if (should_print)
     {
         printf("Num lines: %d\n", total_nums);
         printf("Total sum: %d\n", total_sum);
     }
 
-    // stop timer
+    // stop timer and calculate elapsed time
     gettimeofday(&t2, NULL);
     double t1_s = t1.tv_sec + (t1.tv_usec / 1000000.0);
     double t2_s = t2.tv_sec + (t2.tv_usec / 1000000.0);
@@ -210,9 +224,13 @@ double time_summation(char *filepath, int num_proc, int should_print)
     return elapsed_time;
 }
 
+/* 
+   Add all the numbers contained in the specified portion
+   of the file. Returns the sum.
+ */
 int add_nums(file_portion portion)
 {
-    // open file for reading
+    // open file for reading, checking for errors
     FILE* in_file;
     in_file = fopen(portion.filepath, "r");
     if (in_file == NULL)
@@ -221,18 +239,22 @@ int add_nums(file_portion portion)
         exit(EXIT_FAILURE);
     }
 
-    int sum = 0;
-    char num_string[BYTES_PER_LINE + 1]; // + 1???
-
-    int i;
+    // seek forward to the starting point in the file
     fseek(in_file, portion.offset, SEEK_SET);
+
+    // for each line in the file portion, convert the text to an int and add to total
+    char buffer[BYTES_PER_LINE + 1]; // bytes/line + room for '\0'
+    int sum = 0;
+    int i;
     for (i = 0; i < portion.num_lines; i++)
     {
-        fgets(num_string, BYTES_PER_LINE, in_file);
-        sum += atoi(num_string);
+        fgets(buffer, BYTES_PER_LINE + 1, in_file); // fgets() reads in at most one less than size characters
+        sum += atoi(buffer);
     }
 
+    // close file
     fclose(in_file);
+
     return sum;
 }
 
